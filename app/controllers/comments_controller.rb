f64d7696fb060dfc5babdae27f73a2753190b26d
Aaron Rohrbacher and Noah Gottlieb <aaron+noah@>
2017-11-14 16:57:37 -0800

    fix comments to list

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,22 +2,20 @@ class CommentsController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new
+    render :comments
   end
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comment.new(comment_params)
+    @comment = @post.comments.new(comment_params)
     if @post.save
       redirect_to post_path(@comment.post)
     else
       render :new
     end
-  end
 
-end
 private
   def comment_params
     params.require(:comment).permit(:comment, :post_id)
   end
-
 end
